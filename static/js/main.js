92d75d362de6fc4aac195aab7596d5435c328232
Branislav <branislav@Florians-Mac-mini.local>
2019-01-31 17:24:46 +0100

    update javascript

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,31 @@
-function myFunction() {
+/*
+function toggleFunction() {
     let element = document.querySelector(".navigation__list");
     element.classList.toggle("active");
+}
+*/
+
+var Appradar = {
+	init: function(){
+        var that = this;
+
+        // bind click to element
+        document.querySelector('.js-navicon').addEventListener('click', function(){
+            that.navToggle(), that.changeColor()
+            });
+    },
+    navToggle: function() {
+        let element = document.querySelector(".navigation__list");
+        element.classList.toggle("active");
+    },
+    changeColor: function(){
+        let element2 = document.querySelectorAll(".footernav__link");
+        element2.style.color = "red";
+    }
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', Appradar.init());
+} else {
+  Appradar.init();
 }
